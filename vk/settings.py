3fb519b9318f286a446f7974d91e5b78d225cb93
Danil Vladimirov <danilvladimirovnkk@gmail.com>
2021-06-16 00:09:06 +0300

    fix

--- vk/settings.py
+++ vk/settings.py
@@ -156,9 +156,9 @@ django_heroku.settings(locals())
 LOGIN_URL = 'login_page'
 
 #emails
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'makrosrytp@gmail.com'
 EMAIL_HOST_PASSWORD = 'vla01rov02'
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
